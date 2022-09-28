3cd85dbcb87268ada4446b5ca48fdd08ec637820
Nuno Ferro <nuno.ferro@puregym.com>
2022-09-28 15:18:40 +0100

    1st commit
    1st commit

---
+++ WTest/WTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTestApp.swift
+//  WTest
+//
+//  Created by Nuno Ferro on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
